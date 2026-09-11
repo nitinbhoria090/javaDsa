563c354a185e040c49c35beaafd8b9c07425aeda
nitinbhoria <nitinbhoria090@gmail.com>
2026-09-11 17:18:12 +0530

    Time: 5 ms (82.56%), Space: 46 MB (52.32%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+
+        int reverse = 0;
+        int count = x;
+        while(x != 0){
+            int digit = x % 10;
+            reverse = (reverse * 10) + digit;
+            x /= 10;
+        }
+        return reverse == count;
+    }
+}
